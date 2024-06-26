4c1cad69adb3271d13abcbf2f83e243e77ebd3fe
Drew Marshall <drewwinkles@gmail.com>
2024-06-26 03:36:14 -0500

    Added skeletons for all apps

---
+++ react/app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function App() {
+  return (
+    // This is where all of the app logic should sum up. I want the index.js file to have as minimal code as possible.
+    <div>App</div>
+  )
+}
+
+export default App
